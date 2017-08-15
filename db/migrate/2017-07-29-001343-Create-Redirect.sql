02f4bbf6ce4be0c9d49c092cfd420a686eeab24f
Kenny Grant <kennygrant@gmail.com>
2017-08-15 16:55:13 +0100

    Changed migrations to include redirects table in create tables

--- db/migrate/2017-07-29-001343-Create-Redirect.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS redirects;
-CREATE TABLE redirects (
-id SERIAL NOT NULL,
-created_at timestamp,
-updated_at timestamp,
-new_url text,
-old_url text
-);
-ALTER TABLE redirects OWNER TO fragmentaweb_server;
